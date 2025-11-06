343d6019d745edd074f2a8c561f6b2065e9933a8
Jameslead00 <139333542+Jameslead00@users.noreply.github.com>
2025-11-06 16:02:18 +0000

    Updates

--- custom-service-worker.js
+++ custom-service-worker.js
@@ -1,20 +1,2 @@
-// filepath: src/custom-service-worker.js
-import { registerRoute } from 'https://storage.googleapis.com/workbox-cdn/releases/7.3.0/workbox-routing.js';
-import { StaleWhileRevalidate } from 'https://storage.googleapis.com/workbox-cdn/releases/7.3.0/workbox-strategies.js';
-import { precacheAndRoute } from 'https://storage.googleapis.com/workbox-cdn/releases/7.3.0/workbox-precaching.js';
-
-// Precache alle Assets und index.html
-precacheAndRoute([{"revision":"e67a3782cd55cb52ca500f39ca534f7e","url":"index.html"},{"revision":"e99d788948431329ba24f552b5d0bc64","url":"static/css/main.1299d285.css"},{"revision":"2f19d52fcb243df7a4ea55d5696ccc28","url":"static/js/main.ac554b6f.js"}] || []);
-precacheAndRoute([{ url: '/', revision: null }]);  // Manuell index.html hinzufügen
-
-// Cache-Strategie für Dokumente
-registerRoute(
-  ({ request }) => request.destination === 'document',
-  new StaleWhileRevalidate()
-);
-
-// Optional: Cache für API
-registerRoute(
-  ({ url }) => url.pathname.startsWith('/api'),
-  new StaleWhileRevalidate()
-);
+if(!self.define){let e,s={};const t=(t,o)=>(t=new URL(t+".js",o).href,s[t]||new Promise(s=>{if("document"in self){const e=document.createElement("script");e.src=t,e.onload=s,document.head.appendChild(e)}else e=t,importScripts(t),s()}).then(()=>{let e=s[t];if(!e)throw new Error(`Module ${t} didn’t register its module`);return e}));self.define=(o,i)=>{const r=e||("document"in self?document.currentScript.src:"")||location.href;if(s[r])return;let c={};const n=e=>t(e,r),d={module:{uri:r},exports:c,require:n};s[r]=Promise.all(o.map(e=>d[e]||n(e))).then(e=>(i(...e),c))}}define(["./workbox-3bd9af45"],function(e){"use strict";importScripts("https://storage.googleapis.com/workbox-cdn/releases/7.3.0/workbox-sw.js"),self.addEventListener("message",e=>{e.data&&"SKIP_WAITING"===e.data.type&&self.skipWaiting()}),e.precacheAndRoute([{url:"index.html",revision:"e67a3782cd55cb52ca500f39ca534f7e"},{url:"static/css/main.1299d285.css",revision:"e99d788948431329ba24f552b5d0bc64"},{url:"static/js/main.ac554b6f.js",revision:"2f19d52fcb243df7a4ea55d5696ccc28"}],{}),e.registerRoute(({request:e})=>"document"===e.destination,new e.StaleWhileRevalidate,"GET")});
+//# sourceMappingURL=custom-service-worker.js.map
